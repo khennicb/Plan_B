9922a9e8dcb90011be2edba008f8eaf6167600e5
Colisan <c.loveteit@gmail.com>
2017-05-23 11:03:07 +0200

    correction nextLine scanner

--- src/view/View.java
+++ src/view/View.java
@@ -7,20 +7,22 @@ public abstract class View {
     abstract public void print();
     
     static public int ask(int min, int max){
-        Scanner reader = new Scanner(System.in);
+        Scanner sin = new Scanner(System.in);
         
         int res = min-1;
         boolean ok = false;
         
         while(!ok) {
-            try {
-                res = reader.nextInt();
+            if (sin.hasNextInt()) {
+                res = sin.nextInt();
+                
                 ok = (min <= res && res <= max);
                 
                 if(!ok) {
                     System.out.print("Saisir un nombre entre " + min + " et " + max + " : ");
                 }
-            } catch (Exception e) {
+            } else {
+                sin.nextLine();
                 System.out.print("Saisir un nombre valide : ");
             }
         }
