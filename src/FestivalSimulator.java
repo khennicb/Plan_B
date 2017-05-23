6c6734651bd92ce1c86dbd78fa156905dba0cdb5
Benjamin Khenniche <benjamin.khenniche@ensimag.grenoble-inp.fr>
2017-05-23 10:27:56 +0200

    main added

--- src/FestivalSimulator.java
+++ src/FestivalSimulator.java
@@ -1,5 +1,15 @@
 package src;
+import src.JDBC.*;
+import src.view.*;
+import src.Model.*;
 
 public class FestivalSimulator {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+        
+        
+    }
     
 }
