9f2bee0c95906d1a72c00993f808527234e7c814
Nolwenn Forey <nolwenn.forey@ensimag.grenoble-inp.fr>
2017-05-23 11:52:31 +0200

    add drop table

---
+++ BdD/drop_tables.sql
@@ -0,0 +1,31 @@
+begin;
+
+DROP TABLE Theme;
+
+DROP TABLE Cirque;
+
+DROP TABLE Artiste;
+
+DROP TABLE Expert;
+
+DROP TABLE Artiste_Pseudo;
+
+DROP TABLE Artiste_Telephone;
+
+DROP TABLE Numero;
+
+DROP TABLE Artiste_Participe;
+
+DROP TABLE Artiste_Principal;
+
+DROP TABLE Expert_Evaluation;
+
+DROP TABLE Festival;
+
+DROP TABLE Classement;
+
+DROP TABLE Spectacle;
+
+DROP TABLE Spectacle_Numeros;
+
+rollback;
