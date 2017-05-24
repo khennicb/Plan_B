afd00068a19bb6784d4a23671db30e1712a8377e
Benjamin Khenniche <benjamin.khenniche@ensimag.grenoble-inp.fr>
2017-05-24 10:40:42 +0200

    empty new controller

---
+++ src/controller/ListeSpectacleController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import JDBC.DataBaseManager;
+
+public class ListeSpectacleController implements Controller {
+
+    @Override
+    public void execute() {
+        
+        
+        
+    }
+
+
+    
+}
